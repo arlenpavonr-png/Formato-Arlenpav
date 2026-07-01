5923ce7bbbb9c307829003f1527eea353c6c0a90
ArlenPavon <arlenpavonr@gmail.com>
2026-07-01 16:17:58 -0500

    chore: bump cache v91 (fix buscador cotización)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'arpa-suite-cache-v90';
+const CACHE_NAME = 'arpa-suite-cache-v91';
 const BASE = self.location.pathname.replace(/service-worker\.js$/, '');
 const ASSETS = [
   BASE,
